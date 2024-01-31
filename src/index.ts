e5ab59b9080b7b74dcc87094d7d2a05ed10445b6
htetlinmaung <htetlinmaung@htetlinmaungs-MacBook-Pro-2.local>
2024-01-31 15:32:40 +0630

    update

--- src/index.ts
+++ src/index.ts
@@ -46,7 +46,7 @@ async function main() {
         if (apiData.body) {
           for (const [k, vd] of Object.entries(apiData.body)) {
             const { type, required, response } = vd as IValidation;
-            if ((required && !req.body[k]) || typeof req.body !== type) {
+            if ((required && !req.body[k]) || typeof req.body[k] !== type) {
               if (response.headers) {
                 for (const [hk, hv] of Object.entries(response.headers)) {
                   res.setHeader(hk, hv as string);
